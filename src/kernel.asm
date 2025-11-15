3e8a13ba9d961ffd01ba1cb7b6d9a974421a1b6e
TS prog <sofyan-w@mail.ru>
2025-11-15 18:02:15 +0400

    refactor: extracted print_string function
    BREAKING CHANGE: renamed print_string_vga to print_string

--- src/kernel.asm
+++ src/kernel.asm
@@ -3,6 +3,7 @@ bits 16
 section .text
     global _start
     extern clear_screen
+    extern print_string
 
 ; FIXME: The success_msg is not visible in the output of qemu
 
@@ -11,28 +12,11 @@ _start:
     mov ds, ax
 
     call clear_screen
-    ; call print_string_vga
+
+    mov dx, success_msg
+    mov cx, success_msg_l
+    call print_string
     hlt
-
-; New VGA Text Output Procedure
-print_string_vga:
-    xor si, si
-
-    .next_char:
-        cmp si, success_msg_l
-        je .done
-
-        mov al, [success_msg + si]
-        mov ah, 0x0E  ; BIOS interrupt teletype output
-        mov bh, 0x00  ; page number (usually 0)
-        ; BL = foreground color (optional, often ignored by QEMU's simple VGA)
-        int 0x10 ; Call the BIOS video interrupt
-
-        inc si
-        jmp .next_char
-
-    .done:
-        ret
 
 success_msg db "Kernel successfully loaded", 10
 success_msg_l equ 27
