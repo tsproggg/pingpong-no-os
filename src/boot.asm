7aef2b7c2f9981d2ad7109b9bb7d00aea4ed6f7c
TS prog <sofyan-w@mail.ru>
2025-11-18 18:08:26 +0400

    refactor: moved stack allocation to kernel.asm, replaced the old serial non-working function with the new print_string function

--- src/boot.asm
+++ src/boot.asm
@@ -2,16 +2,14 @@ org 0x7C00
 bits 16
 
 start:
-    cli
-    mov ax, 0x9000      ; Load the desired stack segment value into AX
-    mov ss, ax          ; Set the Stack Segment (SS)
-    mov sp, 0xFFFF      ; Set the Stack Pointer (SP) to the top of the segment
-    sti
-
     mov ax, cs
     mov ds, ax
 
 load_kernel:
+    ; DL must NOT be reset, use the one BIOS gave us
+    ; (it already contains boot drive number)
+    ; DO NOT: mov dl, 0
+
     mov ah, 0x02        ; BIOS read sectors
     mov al, 1           ; number of sectors
     mov dh, 0           ; head 0
@@ -22,10 +20,6 @@ load_kernel:
     mov es, bx
     xor bx, bx
 
-    ; DL must NOT be reset, use the one BIOS gave us
-    ; (it already contains boot drive number)
-    ; DO NOT: mov dl, 0
-
     int 0x13
     jc disk_read_error
 
@@ -33,25 +27,26 @@ load_kernel:
 
 disk_read_error:
 	mov si, DISK_READ_ERROR_MSG
-	call print_string_serial
+
+	.print_string:
+         .next_char:
+             mov al, [si]
+             cmp al, 0
+             je .done
+
+             mov ah, 0x0E  ; BIOS interrupt teletype output
+             mov bh, 0x00  ; page number (usually 0)
+             mov bl, 0x00  ; BL = foreground color (optional, often ignored by QEMU's simple VGA)
+             int 0x10 ; Call the BIOS video interrupt
+
+             inc si
+             jmp .next_char
+
+         .done:
+             ret
+
 	hlt
 
-print_string_serial:
-.next_char:
-    lodsb                   ; AL = [SI], SI++
-    or al, al
-    jz .done
-.wait:
-    mov dx, 0x3FD           ; COM1 Line Status Register (0x3F8 + 5)
-    in al, dx               ; read LSR
-    test al, 0x20           ; check Transmit Holding Empty
-    jz .wait
-    mov dx, 0x3F8           ; COM1 data port
-    mov al, [si-1]          ; get character
-    out dx, al              ; send character
-    jmp .next_char
-.done:
-    ret
 
 DISK_READ_ERROR_MSG db 'DISK_READ_ERROR', 10
 
--- src/kernel.asm
+++ src/kernel.asm
@@ -12,6 +12,12 @@ section .text
 ; FIXME: CPU loses ptr to the .data segment after compilation
 
 _start:
+    cli
+    mov ax, 0x9000      ; Load the desired stack segment value into AX
+    mov ss, ax          ; Set the Stack Segment (SS)
+    mov sp, 0xFFFF      ; Set the Stack Pointer (SP) to the top of the segment
+    sti
+
     mov ax, cs
     mov ds, ax
 
