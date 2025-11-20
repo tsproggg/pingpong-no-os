91eca019ab46e259518455646f3aa76e0551f57e
Menuas <menua_simonian@edu.aua.am>
2025-11-20 21:36:38 +0400

    Added the shutdown function

--- src/kernel.asm
+++ src/kernel.asm
@@ -21,6 +21,7 @@ _start:
 
     mov si, success_msg
     call print_string
+    ;call shutdown
     hlt
 
 ; ---------------------------
@@ -62,3 +63,43 @@ print_string:
 
     .done:
         ret
+
+
+
+
+; ---------------------------
+; |     SUBROUTINE          |
+; ---------------------------
+; Inputs: none
+; Used registers: ax, bx, cx
+shutdown:
+    .do_shutdown:
+
+        ; 1) Connect to APM BIOS
+
+        mov ax, 0x5301
+        xor bx, bx
+        int 0x15
+        jc .apm_fail
+
+
+        ; 2) Enable power management
+
+        mov ax, 0x5308
+        mov bx, 1
+        mov cx, 1
+        int 0x15
+        jc .apm_fail
+
+        ; 3) Turn off the machine
+
+        mov ax, 0x5307
+        mov bx, 1        ; all devices
+        mov cx, 3        ; power-off
+        int 0x15
+        jc .apm_fail
+
+    .apm_fail:
+        cli
+        hlt
+        jmp $
